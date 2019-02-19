a8246243ff9310cf06e1dca2785efea1738206e8
Andrey Popazov <andrey.plagiatix@gmail.com>
2019-02-19 12:53:18 +0200

    Data source splited into separate file

--- Internship/Application/Controllers/Main/HomeViewController.swift
+++ Internship/Application/Controllers/Main/HomeViewController.swift
@@ -16,24 +16,15 @@ class HomeViewController: UIViewController {
 
   private var titleName = "Home"
   
-  private var cellId = "EmployeeCell"
-  private var headerId = "ReusableHeader"
-  
-  var tableView: HomeTableView!
-  lazy var fetchController = DataBaseManager.shared.departmentsFetchController()
-  
-  var data: [SectionData] = []
+  private var source: HomeTableViewDelegates!
   
   override func loadView() {
     super.loadView()
     
-    tableView = HomeTableView(frame: self.view.frame, style: .plain)
+    let tableView = HomeTableView(frame: self.view.frame, style: .plain)
+    source = HomeTableViewDelegates(rootController: self, tableView: tableView)
     
     self.view = tableView
-    
-    tableView?.set(delegate: self, dataSource: self)
-    fetchController.delegate = self
-    DataBaseManager.shared.employeesFetchController().delegate = self
     
     self.navigationItem.title = titleName
     self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(onAddMoreButtonClick))
@@ -41,11 +32,12 @@ class HomeViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    tableView.register(cell: (cellId, EmployeeCell.self), header: (headerId, ReusableHeader.self))
     
-    if let departments = fetchController.fetchedObjects {
+    source.tableView.register(cell: (source.cellId, EmployeeCell.self), header: (source.headerId, ReusableHeader.self))
+    
+    if let departments = source.fetchController.fetchedObjects {
       for department in departments {
-        data.append(.init(department))
+        source.data.append(.init(department))
       }
     }
   }
@@ -54,115 +46,5 @@ class HomeViewController: UIViewController {
     let controller = AddMoreViewController()
     controller.view.backgroundColor = .white
     self.navigationController?.pushViewController(controller, animated: true)
-  }
-}
-
-extension HomeViewController: UITableViewDataSource {
-  func numberOfSections(in tableView: UITableView) -> Int {
-    return data.count
-  }
-  
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    if data[section].isCollapsed {
-      return 0
-    }
-    
-    return data[section].employees.count
-  }
-  
-  func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-    return HomeViewController.defaultSectionHeight
-  }
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! EmployeeCell
-
-    let object = data[indexPath.section]
-    cell.employee = object.employees[indexPath.row]
-    cell.populateTextFields()
-    
-    return cell
-  }
-}
-
-extension HomeViewController: UITableViewDelegate {
-  func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-    let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: headerId) as! ReusableHeader
-    data[section].section = section
-    header.data = data[section]
-    header.delegate = self
-    
-    return header
-  }
-  
-  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    guard let chosenCell = tableView.cellForRow(at: indexPath) as? EmployeeCell else {
-      return
-    }
-
-    let controller = EmployeeProfileController()
-    controller.view.backgroundColor = self.view.backgroundColor
-    controller.profile = chosenCell.employee
-    
-    self.navigationController?.pushViewController(controller, animated: true)
-  }
-  
-  func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-    if (editingStyle == .delete) {
-      DataBaseManager.shared.delete(
-        employeeId: data[indexPath.section].employees[indexPath.row].objectID,
-        fromDepartment: data[indexPath.section].department.objectID
-      )
-      
-      data[indexPath.section].employees.remove(at: indexPath.row)
-      
-      self.tableView!.deleteRows(at: [indexPath], with: .automatic)
-    }
-  }
-}
-
-extension HomeViewController: NSFetchedResultsControllerDelegate {
-  func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-    tableView.beginUpdates()
-  }
-  
-  func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
-    switch type {
-    case .insert:
-      tableView.insertSections(IndexSet(integer: sectionIndex), with: .fade)
-    case .delete:
-      tableView.deleteSections(IndexSet(integer: sectionIndex), with: .fade)
-    case .move:
-      break
-    case .update:
-      break
-    }
-  }
-  
-  func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-    switch type {
-    case .insert:
-      tableView.insertRows(at: [newIndexPath!], with: .fade)
-    case .delete:
-      tableView.deleteRows(at: [indexPath!], with: .fade)
-    case .update:
-      tableView.reloadRows(at: [indexPath!], with: .fade)
-    case .move:
-      tableView.moveRow(at: indexPath!, to: newIndexPath!)
-    }
-  }
-  
-  func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-    tableView.endUpdates()
-  }
-}
-
-extension HomeViewController: ExandableHeaderViewDelegate {
-  func toogleExpand(for header: ReusableHeader, section: Int) {
-    data[section].isCollapsed = !data[section].isCollapsed
-    
-    header.data.isCollapsed = data[section].isCollapsed
-    
-    tableView.reloadSections(NSIndexSet(index: section) as IndexSet, with: .fade)
   }
 }
